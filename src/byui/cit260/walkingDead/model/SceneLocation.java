f8c0c4ead46e7f97e3744f4ba9ffe99f89913f73
laurenkorpacz <kor13003@byui.edu>
2014-05-10 19:51:20 -0600

    hi

---
+++ src/byui/cit260/walkingDead/model/SceneLocation.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.walkingDead.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author laurenkorpacz
+ */
+public class SceneLocation implements Serializable{
+    
+// class instance variables
+private String name;
+private double bestTime;
+
+    public SceneLocation() {
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBestTime() {
+        return bestTime;
+    }
+
+    public void setBestTime(double bestTime) {
+        this.bestTime = bestTime;
+    }
+
+    @Override
+    public String toString() {
+        return "SceneLocation{" + "name=" + name + ", bestTime=" + bestTime + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.bestTime) ^ (Double.doubleToLongBits(this.bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SceneLocation other = (SceneLocation) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.bestTime) != Double.doubleToLongBits(other.bestTime)) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
