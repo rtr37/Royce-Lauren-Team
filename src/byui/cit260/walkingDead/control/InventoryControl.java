e63f684fb0af542682a9a572fe25776b6d3bfe9b
rtr37 <rob11060@byui.edu>
2014-05-21 15:41:55 -0600

    hello

--- src/byui/cit260/walkingDead/control/InventoryControl.java
+++ src/byui/cit260/walkingDead/control/InventoryControl.java
@@ -13,17 +13,15 @@ package byui.cit260.walkingDead.control;
 public class InventoryControl {
     public double calcCompressionofCylinder(double length, double diameter){
     
-    if (length < 1 && length > 10) {
-         return -1;
+    if(length<1 || length>10){
+        return -1;
+    }    
+    if(diameter<1 || diameter>8){
+        return -1;
     }
-            
-    if (diameter < 1 && diameter > 8) {
-         return -1;
-    }
-    double radius = diameter /2;
+    double radius = diameter / 2;
     double volume = (Math.PI * Math.pow(radius,2) * length);
-    
-    double compression = (Math.PI * Math.pow(diameter,2) * length+volume) / volume;
+    double compression = (Math.PI * Math.pow(diameter,2) * length + volume) / volume;
      
     return compression;
     }
