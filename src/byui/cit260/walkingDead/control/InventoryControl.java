1df5469d1d40e0336d1becb1ff8f292a145edb8f
laurenkorpacz <kor13003@byui.edu>
2014-05-22 16:09:47 -0600

    hi

--- src/byui/cit260/walkingDead/control/InventoryControl.java
+++ src/byui/cit260/walkingDead/control/InventoryControl.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package byui.cit260.walkingDead.control;
 
 /**
@@ -11,18 +10,38 @@ package byui.cit260.walkingDead.control;
  * @author rtacklingman50
  */
 public class InventoryControl {
-    public double calcCompressionofCylinder(double length, double diameter){
-    
-    if(length<1 || length>10){
-        return -1;
-    }    
-    if(diameter<1 || diameter>8){
-        return -1;
+
+    public double calcCompressionofCylinder(double length, double diameter) {
+
+        if (length < 1 || length > 10) {
+            return -1;
+        }
+        if (diameter < 1 || diameter > 8) {
+            return -1;
+        }
+        double radius = diameter / 2;
+        double volume = (Math.PI * Math.pow(radius, 2) * length);
+        double compression = (Math.PI * Math.pow(diameter, 2) * length + volume) / volume;
+
+        return compression;
     }
-    double radius = diameter / 2;
-    double volume = (Math.PI * Math.pow(radius,2) * length);
-    double compression = (Math.PI * Math.pow(diameter,2) * length + volume) / volume;
-     
-    return compression;
+
+    public double calcVolumeOfWater(double height, double width, double length) {
+
+        if (height < 0 || height > 12) {
+            return -1;
+        }
+        if (width < 0 || width > 20) {
+            return -2;
+        }
+        if (length < 0 || length > 75) {
+            return -3;
+        }
+
+        double volume = height * width * length; 
+        
+        return volume;
+        
     }
+
 }
