68af6448bb994e98e6ea7d7d759f944b61e50ec9
Lauren <Lauren@10.49.32.117>
2014-06-09 14:49:32 -0600

    yay

---
+++ src/byui/cit260/walkingDead/control/MapControl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.walkingDead.control;
+
+/**
+ *
+ * @author Lauren
+ */
+public class MapControl {
+    
+    static void moveActorsToStartingLocation(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
