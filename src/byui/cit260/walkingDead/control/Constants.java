62802ed9d6fc4a9e00158712fd1e64c4dd85442e
Lauren <Lauren@10.49.32.117>
2014-06-12 15:19:42 -0600

    yay

---
+++ src/byui/cit260/walkingDead/control/Constants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.walkingDead.control;
+
+/**
+ *
+ * @author Lauren
+ */
+class Constants {
+  public final static int NUMBER_OF_ACTORS = 7;
+  public final static int RICK = 5;
+  public final static int CARL= 3;
+  public final static int DARYL = 2;
+  public final static int GLENN = 6;
+  public final static int MAGGIE = 4;
+  public final static int MICHONNE = 1;
+  public final static int CAROL = 7;
+    static int PRISON_SCENE;
+    
+}
