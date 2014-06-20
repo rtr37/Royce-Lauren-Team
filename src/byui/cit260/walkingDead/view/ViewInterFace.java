fc1fff3bc05ca9bc01479260bc319632eb30c196
Lauren <Lauren@10.18.150.177>
2014-06-20 13:16:56 -0600

    yay

---
+++ src/byui/cit260/walkingDead/view/ViewInterFace.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package byui.cit260.walkingDead.view;
+
+/**
+ *
+ * @author Lauren
+ */
+public interface ViewInterFace {
+    public void display();
+    public String getInput();
+    public void doAction(String value);
+    
+}
